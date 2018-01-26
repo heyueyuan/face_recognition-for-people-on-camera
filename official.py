35565181e35f43709bee812128cef37d6b6cf1ca
yueyuan he <heyueyuan901@gmail.com>
2018-01-26 21:48:04 +0800

    Update official.py

--- official.py
+++ official.py
@@ -15,9 +15,6 @@ video_capture = cv2.VideoCapture(0)
 # Load a sample picture and learn how to recognize it.
 obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-123_image = face_recognition.load_image_file("123.jpg")
-123_face_encoding = face_recognition.face_encodings(123_image)[1]
-
 
 while True:
     # Grab a single frame of video
